8c677460aef3b0097e22818d04e2be5ecf596c8d
Max Navsii <ashbay.interactive@gmail.com>
2014-05-15 18:49:48 +0200

    Update Astar file

--- public_html/js/libs/Cellastar/astarNode.js
+++ public_html/js/libs/Cellastar/astarNode.js
@@ -4,4 +4,8 @@
  
  function AstarNode(){
  
+ 	this.x = null;
+ 	this.y = null;
+ 	this.value = null; // <- The value is needed to tell if it's a walkable node or if it's a forbidden node
+ 	this.parent = null;
  };
