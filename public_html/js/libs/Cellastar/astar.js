e2f71bc448667c68cefb761324f963c2b782e08e
Max Navsii <ashbay.interactive@gmail.com>
2014-05-15 19:34:23 +0200

    Fix missing file

---
+++ public_html/js/libs/Cellastar/astar.js
@@ -0,0 +1,15 @@
+/*
+* A* algorithm implementation.
+*/
+ 
+ function Astar(){
+ };
+ 
+ //Compute the best path to go to the destination.
+ //Parameters description :
+ // - map is a Map object it contains all data that the algorithm will use to determine if each point is available for the path destination.
+ // - destinationX and destinationY is the location in the map the player has to go.
+ // - forbidenArea is an array of some values that you decide the player cannot go in (the algorithm will consider these value as forbidden and will make a path avoiding those points)
+ Astar.compute = function(map, destinationX, destinationY, forbiddenArea){
+ 
+ }
